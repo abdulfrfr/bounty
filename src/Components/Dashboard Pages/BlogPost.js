f3e75ace10cb28334f6bfc458fe5fa1aac082cf5
abdulfrfr <abdulmails112@gmail.com>
2023-01-11 11:54:22 +0100

    'updates'

--- src/Components/Dashboard Pages/BlogPost.js
+++ src/Components/Dashboard Pages/BlogPost.js
@@ -10,8 +10,6 @@ function reducer(state, action){
         default:
             break;
     }
-
-
 }
 
 function BlogPost(){
@@ -32,14 +30,14 @@ function BlogPost(){
     
     return(
         <section>
-            <div className="flex justify-center items-center w-[83vw]">
+            <div className="flex justify-center items-center w-[83vw] h-[16vh]">
                 <div className="p-4 flex justify-between items-center w-[80vw] text-gray-700">
                     <div className="text-2xl font-bold">Quick Note</div>
                     <div className="cursor-pointer bg-gray-600 py-2 px-4 text-white rounded-xl">Saved</div>
                 </div>
             </div>
             
-            <div className="bg-white mt-4 w-[83vw] h-[1000vh] px-4 py-4 overflow-y-scroll overflow-hidden lg:overflow-hidden">
+            <div className="bg-white mt-4 w-[83vw] h-[84vh] px-4 pt-4 overflow-y-scroll overflow-x-hidden lg:overflow-hidden">
                 <div>
                     <label className="cursor-pointer flex justity-start items-center text-gray-400" for="image-upload"><MdOutlineImage className='text-xl'/> add cover</label>
                     <input id="image-upload" className="opacity-0 absolute z-[-1]" type='file' name='img' />
@@ -47,13 +45,13 @@ function BlogPost(){
                 <div>
                     <input className="text-5xl w-[81vw] px-4 py-4 outline-none" type='text' name='title' placeholder="Untitle" />
                 </div>
-                <div className='flex justify-center items-center w-[83vw]'>
+                <div className='flex justify-center items-center w-[80vw]'>
                     <div className='w-[60vw]'>
                         { alert.map((alt, index) => {
                             return(
                                 <div className='flex justify-center items-center'>
                                     <div className='flex justify-start items-center'>
-                                        <div onClick={ () => pushAlert(alt)} className='cursor-pointer'>+</div>
+                                        <div onClick={ () => pushAlert(alt)} className='cursor-pointer' >+</div>
                                         <div className='flex justify-start items-start text-gray-800 bg-gray-200 rounded-lg p-4 lg:w-[60vw] w-[80vw] box-border'>
                                             <div>💡</div>
                                             <ContentEditable html={alt} className='outline-none ml-2'/>
--- src/Components/Dashboard Pages/Journal.js
+++ src/Components/Dashboard Pages/Journal.js
@@ -1,6 +1,23 @@
 function Journal(){
     return(
-        <section>Important</section>
+        <section className="md:pt-28 sm:pt-16 pt-5 md:pl-28 sm:pl-10 pl-2 md:pr-10 md:pb-10 w-[83vw] box-border">
+            <div className="">
+                <div className='font-extrabold text-3xl mb-3'>JOURNAL</div>
+                <div className="lg:w-[40vw] md:w-[60vw] sm:w-[50vw] text-md">Document your life, goals and accomplishment
+                to always remind yourslef of all you have gone through
+                and how far you've come.</div>
+            </div>
+            <div className="flex items-center justify-end md:items-end border-b-2 border-gray-700 pb-2 pt-8 pr-4">
+                <div className="text-sm flex items-center justify-between w-[55vw] sm:w-[30vw] md:w-[20vw] lg:w-[15vw]">
+                    <div>Filter</div>
+                    <div>Sort</div>
+                    <div>Search</div>
+                    <div>
+                        <button className="bg-blue-600 py-1 px-2 rounded-md">New</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 
 }
